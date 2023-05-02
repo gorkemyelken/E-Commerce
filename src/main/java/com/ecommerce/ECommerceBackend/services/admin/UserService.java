046a7c43f85c2dca70a6b757287407f8b8a2b6d8
Görkem <gorkem_yelken@hotmail.com>
2023-05-02 21:34:17 +0300

    New User Function

--- src/main/java/com/ecommerce/ECommerceBackend/services/admin/UserService.java
+++ src/main/java/com/ecommerce/ECommerceBackend/services/admin/UserService.java
@@ -1,6 +1,8 @@
 package com.ecommerce.ECommerceBackend.services.admin;
 
+import com.ecommerce.ECommerceBackend.entities.admin.Role;
 import com.ecommerce.ECommerceBackend.entities.admin.User;
+import com.ecommerce.ECommerceBackend.repositories.admin.RoleRepository;
 import com.ecommerce.ECommerceBackend.repositories.admin.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,10 +16,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> listAll() {
-        List<User> userList = new ArrayList<>();
-        userRepository.findAll().forEach(userList::add);
-        return userList;
+        return (List<User>) userRepository.findAll();
     }
 
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
