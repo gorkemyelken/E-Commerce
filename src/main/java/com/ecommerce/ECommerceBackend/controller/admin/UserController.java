117312139e83d5bbd92680482881090d49105bbe
Görkem <gorkem_yelken@hotmail.com>
2023-05-02 20:36:50 +0300

    Users Listing Function

--- src/main/java/com/ecommerce/ECommerceBackend/controller/admin/UserController.java
+++ src/main/java/com/ecommerce/ECommerceBackend/controller/admin/UserController.java
@@ -3,6 +3,7 @@ package com.ecommerce.ECommerceBackend.controller.admin;
 import com.ecommerce.ECommerceBackend.entities.admin.User;
 import com.ecommerce.ECommerceBackend.services.admin.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,10 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
-    public List<User> listAll(){
-        return userService.listAll();
+    public String listAll(Model model) {
+        List<User> listUsers = userService.listAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
     }
 }
